72bb2df1818b00d301703f2a3d01a777eb04e6cf
lyhhh <17610603221@163.com>
2020-10-06 19:08:42 +0800

    添加 github 链接

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <h1>轱辘UI</h1>
       <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
-        <a>GitHub</a>
+        <a href="https://github.com/EvaLLLLL/gulu-ui">GitHub</a>
         <a>开始</a>
       </p>
     </div>
