d3c05ba2aaf15f4e0ea7dc05df3f39a239ea1ac4
lyhhh <17610603221@163.com>
2020-10-06 19:07:32 +0800

    完成 Home 页面 CSS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
     <Topnav/>
-    </div>
     <div class="banner">
       <h1>轱辘UI</h1>
       <h2>一个厉害的 UI 框架</h2>
@@ -10,7 +8,6 @@
         <a>开始</a>
       </p>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -21,4 +18,25 @@ export default {
 </script>
 
 <style lang="scss">
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
