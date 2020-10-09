5ede09e42bc128c3d1377c42efdb6e7f5a33878f
lyhhh <17610603221@163.com>
2020-10-10 01:44:06 +0800

    完成圆弧

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -45,6 +45,9 @@
 	
 	.topnavAndBanner {
 		background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+		/*border-bottom-left-radius: 50% 50px;*/
+		/*border-bottom-right-radius: 50% 50px;*/
+		clip-path: ellipse(80% 60% at 50% 40%);
 	}
 	.banner {
 		color: $color;
