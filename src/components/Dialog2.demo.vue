6cd67f75af0cadedb98ccdb4b4bfed4de8c9fb9a
lyhhh <17610603221@163.com>
2020-10-11 00:05:33 +0800

    改造 DialogDemo

---
+++ src/components/Dialog2.demo.vue
@@ -0,0 +1,31 @@
+<demo>
+	示例二
+</demo>
+
+<template>
+	<Button @click="showDialog">show</Button>
+</template>
+
+<script lang="ts">
+	import {openDialog} from '../lib/openDialog';
+	import Button from '../lib/Button.vue';
+	
+	export default {
+		components: {Button},
+		setup() {
+			const showDialog = () => {
+				openDialog({
+					title: '标题',
+					content: '内容内容内容内容内容内容内容内容',
+					ok() {
+						console.log('ok');
+					},
+					cancel() {
+						console.log('cancel');
+					},
+				});
+			};
+			return {showDialog};
+		},
+	};
+</script>
