76bf025d8c82ae742c90f2f6bf06e37d93219c11
lyhhh <17610603221@163.com>
2020-10-11 02:20:17 +0800

    将 md 文件修改为静态加载

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import {createWebHashHistory, createRouter} from 'vue-router';
+const history = createWebHashHistory();
 // @ts-ignore
 import Home from './views/Home.vue';
 // @ts-ignore
@@ -15,10 +16,15 @@ import TabsDemo from './components/TabsDemo.vue';
 import {h} from 'vue';
 // @ts-ignore
 import Markdown from './components/Markdown.vue';
+// @ts-ignore
+import intro from './markdown/intro.md';
+// @ts-ignore
+import getStarted from './markdown/get-started.md';
+// @ts-ignore
+import install from './markdown/install.md';
 
-const history = createWebHashHistory();
-const md = (fileName) => {
-	return h(Markdown, {path: '../markdown/' + fileName + '.md', key: fileName});
+const md = (string) => {
+	return h(Markdown, {content: string, key: string});
 };
 
 export const router = createRouter({
@@ -28,9 +34,9 @@ export const router = createRouter({
 		{
 			path: '/doc', component: Doc, children: [
 				{path: '', redirect: '/doc/intro'},
-				{path: 'intro', component: md('intro')},
-				{path: 'get-started', component: md('get-started')},
-				{path: 'install', component: md('install')},
+				{path: 'intro', component: md(intro)},
+				{path: 'get-started', component: md(getStarted)},
+				{path: 'install', component: md(install)},
 				{path: 'switch', component: SwitchDemo},
 				{path: 'button', component: ButtonDemo},
 				{path: 'dialog', component: DialogDemo},
